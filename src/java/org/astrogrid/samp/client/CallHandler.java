1d324db0533b70692df6e34b6c31cbbfb6d4058f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-06-11 17:55:53 +0000

    MessageHandler utility implementation
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@23420 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/client/CallHandler.java
@@ -0,0 +1,75 @@
+package org.astrogrid.samp.client;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.astrogrid.samp.ErrInfo;
+import org.astrogrid.samp.Message;
+import org.astrogrid.samp.Response;
+import org.astrogrid.samp.SampException;
+import org.astrogrid.samp.Subscriptions;
+
+public abstract class CallHandler implements MessageHandler {
+
+    private Subscriptions subscriptions_;
+    private final Logger logger_ =
+        Logger.getLogger( CallHandler.class.getName() );
+
+    public CallHandler( Map subscriptions ) {
+        setSubscriptions( subscriptions );
+    }
+
+    public CallHandler( String[] mtypes ) {
+        Map subs = new HashMap();
+        for ( int i = 0; i < mtypes.length; i++ ) {
+            subs.put( mtypes[ i ], new HashMap() );
+        }
+        setSubscriptions( subs );
+    }
+
+    public CallHandler( String mtype ) {
+        this( new String[] { mtype } );
+    }
+
+    public void setSubscriptions( Map subscriptions ) {
+        Subscriptions subs = Subscriptions.asSubscriptions( subscriptions );
+        subs.check();
+        subscriptions_ = subs;
+    }
+
+    public Map getSubscriptions() {
+        return subscriptions_;
+    }
+
+    public abstract Map processCall( HubConnection connection, String senderId,
+                                     Message message )
+        throws Exception;
+
+    public void receiveNotification( HubConnection connection, String senderId,
+                                     Message message ) {
+        try {
+            processCall( connection, senderId, message );
+        }
+        catch ( Exception e ) {
+            logger_.log( Level.INFO,
+                         "Error processing notification " + message.getMType()
+                       + " - ignored", e );
+        }
+    }
+
+    public void receiveCall( HubConnection connection, String senderId,
+                             String msgId, Message message )
+            throws SampException {
+        Response response;
+        try {
+            Map result = processCall( connection, senderId, message ); 
+            result = result == null ? new HashMap() : result;
+            response = Response.createSuccessResponse( result );
+        }
+        catch ( Exception e ) {
+            response = Response.createErrorResponse( new ErrInfo( e ) );
+        }
+        connection.reply( msgId, response );
+    }
+}
