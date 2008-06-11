4374bffbd605d8d4eb45a3b351c84c0e8983f024
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-06-11 14:50:49 +0000

    Add utility methods
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@23411 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/RegInfo.java
+++ src/java/org/astrogrid/samp/RegInfo.java
@@ -1,10 +1,19 @@
 package org.astrogrid.samp;
 
+import java.util.Map;
+
 public class RegInfo extends SampMap {
 
     public static final String HUBID_KEY = "samp.hub-id";
     public static final String SELFID_KEY = "samp.self-id";
     public static final String PRIVATEKEY_KEY = "samp.private-key";
+
+    public RegInfo() {
+    }
+
+    public RegInfo( Map map ) {
+        super( map );
+    }
 
     public RegInfo( String hubId, String selfId, String privateKey ) {
         put( HUBID_KEY, hubId );
@@ -22,5 +31,11 @@ public class RegInfo extends SampMap {
 
     public String getPrivateKey() {
         return getString( PRIVATEKEY_KEY );
+    }
+
+    public static RegInfo asRegInfo( Map map ) {
+        return map instanceof RegInfo
+             ? (RegInfo) map
+             : new RegInfo( map );
     }
 }
