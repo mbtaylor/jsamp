2bea7754a54eb52620ddddbafd32f62fa76e71a8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-06-11 14:57:03 +0000

    Renamed XmlRpcHub -> HubXmlRpcHandler (and reimplemented)
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@23413 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/hub/HubXmlRpcHandler.java
@@ -0,0 +1,81 @@
+package org.astrogrid.samp.hub;
+
+import java.util.List;
+import java.util.Map;
+import org.astrogrid.samp.SampXmlRpcHandler;
+import org.astrogrid.samp.SampException;
+
+public class HubXmlRpcHandler extends SampXmlRpcHandler {
+
+    public HubXmlRpcHandler( HubService service ) {
+        super( "samp.hub", HubActor.class, new HubActorImpl( service ) );
+    }
+
+    private static class HubActorImpl implements HubActor {
+        private final HubService service_;
+
+        HubActorImpl( HubService service ) {
+            service_ = service;
+        }
+        public void isAlive() {
+        }
+        public void isAlive( String privateKey ) {
+        }
+        public Map register( String secret ) throws SampException {
+            return service_.register( secret );
+        }
+        public void unregister( String privateKey ) throws SampException {
+            service_.unregister( privateKey );
+        }
+        public void declareMetadata( String privateKey, Map metadata )
+                throws SampException {
+            service_.declareMetadata( privateKey, metadata );
+        }
+        public Map getMetadata( String privateKey, String clientId ) 
+                throws SampException {
+            return service_.getMetadata( privateKey, clientId );
+        }
+        public void declareSubscriptions( String privateKey, Map subs ) 
+                throws SampException {
+            service_.declareSubscriptions( privateKey, subs );
+        }
+        public Map getSubscriptions( String privateKey, String clientId ) 
+                throws SampException {
+            return service_.getSubscriptions( privateKey, clientId );
+        }
+        public List getRegisteredClients( String privateKey ) 
+                throws SampException {
+            return service_.getRegisteredClients( privateKey );
+        }
+        public Map getSubscribedClients( String privateKey, String mtype ) 
+                throws SampException {
+            return service_.getSubscribedClients( privateKey, mtype );
+        }
+        public void notify( String privateKey, String recipientId, Map msg ) 
+                throws SampException {
+            service_.notify( privateKey, recipientId, msg );
+        }
+        public void notifyAll( String privateKey, Map msg ) 
+                throws SampException {
+            service_.notifyAll( privateKey, msg );
+        }
+        public String call( String privateKey, String recipientId,
+                            String msgTag, Map msg ) throws SampException {
+            return service_.call( privateKey, recipientId, msgTag, msg );
+        }
+        public String callAll( String privateKey, String msgTag, Map msg ) 
+                throws SampException {
+            return service_.callAll( privateKey, msgTag, msg );
+        }
+        public Map callAndWait( String privateKey, String recipientId, Map msg,
+                                String timeout ) 
+                throws SampException {
+            return service_.callAndWait( privateKey, recipientId, msg,
+                                         timeout );
+        }
+        public void reply( String privateKey, String msgId, Map response ) 
+                throws SampException {
+            service_.reply( privateKey, msgId, response );
+        }
+    }
+}
