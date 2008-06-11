0e11dfcb248342f1f963185be56eb579777ea539
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-06-11 15:28:22 +0000

    XmlRpc hub interface
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@23415 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/hub/HubActor.java
@@ -0,0 +1,41 @@
+package org.astrogrid.samp.hub;
+
+import java.util.List;
+import java.util.Map;
+import org.astrogrid.samp.SampException;
+
+/**
+ * This interface is an implementation detail, but it has to be visible 
+ * because it's used in another package.  Rats.
+ */
+public interface HubActor {
+    void isAlive();
+    void isAlive( String privateKey );
+    Map register( String secret ) throws SampException;
+    void unregister( String privateKey ) throws SampException;
+    void declareMetadata( String privateKey, Map metadata )
+            throws SampException;
+    Map getMetadata( String privateKey, String clientId )
+            throws SampException;
+    void declareSubscriptions( String privateKey, Map subs )
+            throws SampException;
+    Map getSubscriptions( String privateKey, String clientId )
+            throws SampException;
+    List getRegisteredClients( String privateKey )
+            throws SampException;
+    Map getSubscribedClients( String privateKey, String mtype )
+            throws SampException;
+    void notify( String privateKey, String recipientId, Map msg )
+            throws SampException;
+    void notifyAll( String privateKey, Map msg )
+            throws SampException;
+    String call( String privateKey, String recipientId, String msgTag, Map msg )
+            throws SampException;
+    String callAll( String privateKey, String msgTag, Map msg )
+            throws SampException;
+    Map callAndWait( String privateKey, String recipientId, Map msg,
+                     String timeout )
+            throws SampException;
+    void reply( String privateKey, String msgId, Map response )
+            throws SampException;
+}
