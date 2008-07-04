63f7f00c3993e53bf404d38677ff531575a5e73f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-07-04 14:19:46 +0000

    More singing, more dancing
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@23569 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/gui/ClientListCellRenderer.java
+++ src/java/org/astrogrid/samp/gui/ClientListCellRenderer.java
@@ -1,19 +1,24 @@
 package org.astrogrid.samp.gui;
 
 import java.awt.Component;
+import java.awt.Font;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListModel;
 import org.astrogrid.samp.Client;
+import org.astrogrid.samp.RegInfo;
 
 public class ClientListCellRenderer extends DefaultListCellRenderer {
 
     private final ClientLabeller labeller_;
-    private ListModel listModel_;
+    private final RegInfo regInfo_;
+    private Font[] labelFonts_;
 
-    public ClientListCellRenderer() {
+    public ClientListCellRenderer( ListModel listModel, RegInfo regInfo ) {
         labeller_ = new ClientLabeller();
+        labeller_.setClientListModel( listModel );
+        regInfo_ = regInfo;
     }
 
     public Component getListCellRendererComponent( JList list, Object value,
@@ -24,14 +29,31 @@ public class ClientListCellRenderer extends DefaultListCellRenderer {
         if ( c instanceof JLabel && value instanceof Client ) {
             JLabel jl = (JLabel) c;
             Client client = (Client) value;
-            ListModel model = list.getModel();
-            if ( model != listModel_ ) {
-                labeller_.setClientListModel( model );
-                listModel_ = model;
+            String id = client.getId();
+            String label = labeller_.getLabel( client );
+            String text = label == null ? id
+                                        : label;
+            if ( regInfo_ != null ) {
+                if ( id.equals( regInfo_.getHubId() ) ) {
+                    text += " (hub)";
+                }
+                else if ( id.equals( regInfo_.getSelfId() ) ) {
+                    text += " (self)";
+                }
             }
-            jl.setText( labeller_.getLabel( client ) );
+            jl.setText( text );
+            jl.setFont( getLabelFont( label == null ) );
             jl.setIcon( labeller_.getIcon( client ) );
         }
         return c;
+    }
+
+    private Font getLabelFont( boolean isAlias ) {
+        if ( labelFonts_ == null ) {
+            Font normalFont = getFont();
+            Font aliasFont = getFont().deriveFont( Font.ITALIC );
+            labelFonts_ = new Font[] { normalFont, aliasFont };
+        }
+        return labelFonts_[ isAlias ? 1 : 0 ];
     }
 }
