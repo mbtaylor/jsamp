cec280c85ffaaeadbac1c3b7a5a7787890891583
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-07-31 13:06:03 +0000

    Rename Samp->JSamp
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@23928 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/test/java/org/astrogrid/samp/CommandTest.java
+++ src/test/java/org/astrogrid/samp/CommandTest.java
@@ -9,11 +9,16 @@ public class CommandTest extends TestCase {
     }
 
     public void testCommands() throws Exception {
-        String[] cmdClasses = Samp.COMMAND_CLASSES;
+        String[] cmdClasses = JSamp.COMMAND_CLASSES;
         for ( int i = 0; i < cmdClasses.length; i++ ) {
             String className = cmdClasses[ i ];
             Class clazz = Class.forName( className );
-            assertNotNull( Samp.getMainMethod( clazz ) );
+            assertNotNull( JSamp.getMainMethod( clazz ) );
         }
+    }
+
+    public void testVersion() {
+        assertTrue( SampUtils.getSampVersion().trim().charAt( 0 ) != '?' );
+        assertTrue( SampUtils.getSoftwareVersion().trim().charAt( 0 ) != '?' );
     }
 }
