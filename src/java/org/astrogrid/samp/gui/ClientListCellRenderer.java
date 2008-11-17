ee485165cb79df526fe595c2d87a1c10370e8a8c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-11-17 13:32:11 +0000

    Icon handling now farmed out to other classes
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@24787 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/gui/ClientListCellRenderer.java
+++ src/java/org/astrogrid/samp/gui/ClientListCellRenderer.java
@@ -2,9 +2,7 @@ package org.astrogrid.samp.gui;
 
 import java.awt.Component;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.Icon;
 import javax.swing.JLabel;
@@ -25,6 +23,7 @@ public class ClientListCellRenderer extends DefaultListCellRenderer {
     private final ClientLabeller labeller_;
     private boolean useNicknames_;
     private Font[] labelFonts_;
+    private IconStore iconStore_;
 
     /**
      * Constructor.
@@ -35,6 +34,7 @@ public class ClientListCellRenderer extends DefaultListCellRenderer {
      */
     public ClientListCellRenderer( ListModel listModel, RegInfo regInfo ) {
         labeller_ = new ClientLabeller( listModel, regInfo );
+        iconStore_ = new IconStore( -1, IconStore.createEmptyIcon( 16 ) );
     }
 
     /**
@@ -95,7 +95,8 @@ public class ClientListCellRenderer extends DefaultListCellRenderer {
             }
             jl.setText( text );
             jl.setFont( font );
-            jl.setIcon( sizeIcon( labeller_.getIcon( client ), size ) );
+            jl.setIcon( SizedIcon.sizeIcon( iconStore_.getIcon( client ),
+                                            size ) );
         }
         return c;
     }
@@ -113,84 +114,5 @@ public class ClientListCellRenderer extends DefaultListCellRenderer {
             labelFonts_ = new Font[] { normalFont, aliasFont };
         }
         return labelFonts_[ special ? 1 : 0 ];
-    }
-
-    /**
-     * Return an icon based on an existing one, but drawn to an exact size.
-     *
-     * @param  icon  original icon, or null for blank
-     * @param  size  number of horizontal and vertical pixels in output
-     * @return  resized version of <code>icon</code>
-     */
-    private static Icon sizeIcon( Icon icon, final int size ) {
-        if ( icon == null ) {
-            return new Icon() {
-                public int getIconWidth() {
-                    return size;
-                }
-                public int getIconHeight() {
-                    return size;
-                }
-                public void paintIcon( Component c, Graphics g, int x, int y ) {
-                }
-            };
-        }
-        else if ( icon.getIconWidth() == size &&
-                  icon.getIconHeight() == size ) {
-            return icon;
-        }
-        else {
-            return new SizedIcon( icon, size );
-        }
-    }
-
-    /**
-     * Icon implementation which looks like an existing one, but is resized
-     * down if necessary.
-     */
-    private static class SizedIcon implements Icon {
-        private final Icon icon_;
-        private final int size_;
-        private final double factor_;
-
-        /**
-         * Constructor.
-         *
-         * @param   icon  original icon
-         * @param   size  number of horizontal and vertical pixels in this icon
-         */
-        public SizedIcon( Icon icon, int size ) {
-            icon_ = icon;
-            size_ = size;
-            factor_ =
-                Math.min( 1.0,
-                          Math.min( size / (double) icon.getIconWidth(),
-                                    size / (double) icon.getIconHeight() ) );
-        }
-
-        public int getIconWidth() {
-            return size_;
-        }
-
-        public int getIconHeight() {
-            return size_;
-        }
-
-        public void paintIcon( Component c, Graphics g, int x, int y ) {
-            int iw = icon_.getIconWidth();
-            int ih = icon_.getIconHeight();
-            if ( factor_ == 1.0 ) {
-                icon_.paintIcon( c, g, ( size_ - iw ) / 2, ( size_ - ih ) / 2 );
-            }
-            else {
-                Graphics2D g2 = (Graphics2D) g;
-                AffineTransform trans = g2.getTransform();
-                g2.translate( ( size_ - iw * factor_ ) / 2,
-                              ( size_ - ih * factor_ ) / 2 );
-                g2.scale( factor_, factor_ );
-                icon_.paintIcon( c, g2, x, y );
-                g2.setTransform( trans );
-            }
-        }
     }
 }
