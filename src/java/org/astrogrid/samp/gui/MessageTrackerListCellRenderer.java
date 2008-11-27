c00bb59d23fe1918a84ccfda5f3af2bce99f499e
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-11-27 13:42:59 +0000

    Rewrite.  No longer uses TransmissionListIcon
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@24897 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/gui/MessageTrackerListCellRenderer.java
+++ src/java/org/astrogrid/samp/gui/MessageTrackerListCellRenderer.java
@@ -6,11 +6,17 @@ import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JComponent;
 import javax.swing.JList;
-import javax.swing.ToolTipManager;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 import org.astrogrid.samp.Client;
-import org.astrogrid.samp.SampUtils;
 
 /**
  * ListCellRenderer which draws a representation of incoming and outgoing
@@ -22,9 +28,11 @@ import org.astrogrid.samp.SampUtils;
 class MessageTrackerListCellRenderer extends ClientListCellRenderer {
 
     private final ClientTransmissionHolder transHolder_;
-    private final int padding_;
-    private Client client_;
-    private TransmissionListIcon transListIcon_;
+    private final int msgGap_;
+    private final IconBox iconBox_;
+    private final IconListModel iconListModel_;
+    private final Object separator_;
+    private final int border_;
 
     /**
      * Constructor.
@@ -35,40 +43,17 @@ class MessageTrackerListCellRenderer extends ClientListCellRenderer {
     public MessageTrackerListCellRenderer(
             ClientTransmissionHolder transHolder ) {
         transHolder_ = transHolder;
-        padding_ = 10;
-    }
-
-    protected void paintComponent( Graphics g ) {
-        super.paintComponent( g );
-        if ( transListIcon_ != null ) {
-            Point p = getIconPosition();
-            if ( g.hitClip( p.x, p.y, transListIcon_.getIconWidth(),
-                                      transListIcon_.getIconHeight() ) ) {
-                transListIcon_.paintIcon( this, g, p.x, p.y );
-            }
-        }
-    }
-
-    public Dimension getPreferredSize() {
-        Dimension prefSize = super.getPreferredSize();
-        if ( transListIcon_ != null ) {
-            prefSize.width += transListIcon_.getIconWidth() + padding_;
-        }
-        return prefSize;
-    }
-
-    public String getToolTipText( MouseEvent evt ) {
-        if ( transListIcon_ != null ) {
-            Point iconPos = getIconPosition();
-            Point p = new Point( evt.getPoint() );
-            p.x -= iconPos.x;
-            p.y -= iconPos.y;
-            Transmission trans = transListIcon_.getTransmissionAt( p );
-            if ( trans != null ) {
-                return getToolTipText( trans );
-            }
-        }
-        return null;
+        iconListModel_ = new IconListModel();
+        msgGap_ = 10;
+        separator_ = new Object();
+        border_ = 1;
+        iconBox_ = new IconBox( false, 16 );
+        iconBox_.setOpaque( false );
+        iconBox_.setBorder( BorderFactory
+                           .createEmptyBorder( border_, border_,
+                                               border_, border_ ) );
+        iconBox_.setModel( iconListModel_ );
+        iconBox_.setRenderer( new IconBoxRenderer() );
     }
 
     /**
@@ -79,18 +64,47 @@ class MessageTrackerListCellRenderer extends ClientListCellRenderer {
      */
     protected String getToolTipText( Transmission trans ) {
         String mtype = trans.getMessage().getMType();
-        if ( client_ == trans.getSender() ) {
+        Client client = iconListModel_.client_;
+        if ( client == trans.getSender() ) {
             return mtype + " -> "
-                 + SampUtils.toString( trans.getReceiver() );
+                 + org.astrogrid.samp.SampUtils.toString( trans.getReceiver() );
         }
-        else if ( client_ == trans.getReceiver() ) {
+        else if ( client == trans.getReceiver() ) {
             return mtype + " <- "
-                 + SampUtils.toString( trans.getSender() );
+                 + org.astrogrid.samp.SampUtils.toString( trans.getSender() );
         }
         else {
             assert false;
             return null;
         }
+    }
+
+    protected void paintComponent( Graphics g ) {
+        super.paintComponent( g );
+        if ( iconListModel_.client_ != null ) {
+            Point p = getIconBoxPosition();
+            Dimension boxSize = iconBox_.getPreferredSize();
+            if ( g.hitClip( p.x, p.y, boxSize.width, boxSize.height ) ) {
+                g.translate( p.x, p.y );
+                iconBox_.paintComponent( g );
+                g.translate( -p.x, -p.y );
+            }
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        Dimension prefSize = super.getPreferredSize();
+        if ( iconListModel_.client_ != null ) {
+            Dimension boxSize = iconBox_.getPreferredSize();
+            prefSize.width += iconBox_.getPreferredSize().width + msgGap_;
+        }
+        return prefSize;
+    }
+
+    public String getToolTipText( MouseEvent evt ) {
+        Point boxPos = getIconBoxPosition();
+        evt.translatePoint( -boxPos.x, -boxPos.y );
+        return iconBox_.getToolTipText( evt );
     }
 
     public Component getListCellRendererComponent( JList list, Object value,
@@ -99,20 +113,16 @@ class MessageTrackerListCellRenderer extends ClientListCellRenderer {
         Component c = super.getListCellRendererComponent( list, value, index,
                                                           isSel, hasFocus );
         if ( value instanceof Client ) {
-            client_ = (Client) value;
+            iconListModel_.setClient( (Client) value );
             int size = c.getPreferredSize().height;
             if ( c instanceof JComponent ) {
                 Insets insets = ((JComponent) c).getInsets();
                 size -= insets.top + insets.bottom;
             }
-            transListIcon_ =
-                new TransmissionListIcon( transHolder_
-                                         .getRxListModel( client_ ),
-                                          transHolder_
-                                         .getTxListModel( client_ ), size );
+            iconBox_.setTransverseSize( size );
         }
         else {
-            transListIcon_ = null;
+            iconListModel_.setClient( null );
         }
         return c;
     }
@@ -123,10 +133,224 @@ class MessageTrackerListCellRenderer extends ClientListCellRenderer {
      *
      * @return   icon base position
      */
-    private Point getIconPosition() {
+    private Point getIconBoxPosition() {
         Insets insets = getInsets();
         return new Point( insets.left + super.getPreferredSize().width
-                                      + padding_,
+                                      + msgGap_,
                           insets.top );
+    }
+
+    /**
+     * ListModel which can be used in the icon box.
+     * It contains entries for each received and sent message, as well as
+     * one which notionally represents the application (visual sugar).
+     * It is basically a combination of the rx and tx models.
+     */
+    private class IconListModel implements ListModel {
+        Client client_;
+        ListModel rxModel_;
+        ListModel txModel_;
+        private final ListDataListener rxListener_;
+        private final ListDataListener txListener_;
+        private final List listenerList_;
+
+        /**
+         * Constructor.
+         */
+        IconListModel() {
+            listenerList_ = new ArrayList();
+            rxListener_ = new ListDataForwarder() {
+                public int getOffset() {
+                    return 0;
+                }
+            };
+            txListener_ = new ListDataForwarder() {
+                public int getOffset() {
+                    return ( rxModel_ == null ? 0 : rxModel_.getSize() ) + 1;
+                }
+            };
+        }
+
+        /**
+         * Sets the client whose transmissions this list will represent.
+         * May be null.
+         *
+         * @param   client  client
+         */
+        public void setClient( Client client ) {
+            if ( rxModel_ != null ) {
+                rxModel_.removeListDataListener( rxListener_ );
+            }
+            if ( txModel_ != null ) {
+                txModel_.removeListDataListener( txListener_ );
+            }
+            client_ = client;
+            rxModel_ = transHolder_.getRxListModel( client );
+            txModel_ = transHolder_.getTxListModel( client );
+            if ( rxModel_ != null ) {
+                rxModel_.addListDataListener( rxListener_ );
+            }
+            if ( txModel_ != null ) {
+                txModel_.addListDataListener( txListener_ );
+            }
+            fireEvent( new ListDataEvent( this, ListDataEvent.CONTENTS_CHANGED,
+                                          -1, -1 ) );
+        }
+
+        public int getSize() {
+            return ( rxModel_ == null ? 0 : rxModel_.getSize() )
+                 + 1
+                 + ( txModel_ == null ? 0 : txModel_.getSize() );
+        }
+
+        public Object getElementAt( int index ) {
+            int rxSize = rxModel_ == null ? 0 : rxModel_.getSize();
+            if ( index < rxSize ) {
+                return rxModel_.getElementAt( index );
+            }
+            index -= rxSize;
+
+            if ( index < 1 ) {
+                return separator_;
+            }
+            index -= 1;
+
+            int txSize = txModel_ == null ? 0 : txModel_.getSize();
+            if ( index < txSize ) {
+                return txModel_.getElementAt( index );
+            }
+            index -= txSize;
+
+            throw new IllegalArgumentException();
+        }
+
+        public void addListDataListener( ListDataListener listener ) {
+            listenerList_.add( listener );
+        }
+
+        public void removeListDataListener( ListDataListener listener ) {
+            listenerList_.remove( listener );
+        }
+
+        /**
+         * Passes an event on to registered ListDataListeners.
+         */
+        private void fireEvent( ListDataEvent evt ) {
+            for ( Iterator it = listenerList_.iterator(); it.hasNext(); ) {
+                ListDataListener listener = (ListDataListener) it.next();
+                switch ( evt.getType() ) {
+                    case ListDataEvent.INTERVAL_ADDED:
+                        listener.intervalAdded( evt );
+                        break;
+                    case ListDataEvent.INTERVAL_REMOVED:
+                        listener.intervalRemoved( evt );
+                        break;
+                    case ListDataEvent.CONTENTS_CHANGED:
+                        listener.contentsChanged( evt );
+                        break;
+                    default:
+                        assert false;
+                }
+            }
+        }
+
+        /**
+         * Listener implementation which can listen to constituent (rx and tx)
+         * models and forward events from them to listeners to this model.
+         */
+        private abstract class ListDataForwarder implements ListDataListener {
+
+            /**
+             * Returns the offset into the IconBoxModel at which the
+             * model this listener is listening to starts.
+             *
+             * @return  model element offset
+             */
+            abstract int getOffset();
+
+            public void intervalAdded( ListDataEvent evt ) {
+                forwardEvent( evt );
+            }
+
+            public void intervalRemoved( ListDataEvent evt ) {
+                forwardEvent( evt );
+            }
+
+            public void contentsChanged( ListDataEvent evt ) {
+                forwardEvent( evt );
+            }
+
+            /**
+             * Takes an event received by this listener, adjusts its
+             * indexes appropriately, and forwards it to listeners to this
+             * model.
+             *
+             * @param  evt   event to forward
+             */
+            private void forwardEvent( ListDataEvent evt ) {
+                Object src = evt.getSource();
+                int i0 = evt.getIndex0();
+                int i1 = evt.getIndex1();
+                if ( 0 <= i0 && i0 <= i1 ) {
+                    int offset = getOffset();
+                    fireEvent( new ListDataEvent( evt.getSource(),
+                                                  evt.getType(),
+                                                  evt.getIndex0() + offset,
+                                                  evt.getIndex1() + offset ) );
+                }
+                else {
+                    fireEvent( evt );
+                }
+            }
+        }
+    }
+
+    private class IconBoxRenderer implements IconBox.CellRenderer {
+        public Icon getIcon( final Object value ) {
+            final int height = iconBox_.getTransverseSize() - border_ * 2;
+            if ( value == separator_ ) {
+                return new Icon() {
+                    public void paintIcon( Component c, Graphics g,
+                                           int x, int y ) {
+                        g.drawOval( x, y, height, height );
+                    }
+                    public int getIconWidth() {
+                        return height;
+                    }
+                    public int getIconHeight() {
+                        return height;
+                    }
+                };
+            }
+            else {
+                final int width = (int) Math.floor( 0.866 * height );
+                return new Icon() {
+                    public void paintIcon( Component c, Graphics g,
+                                           int x, int y ) {
+                        int xlo = x;
+                        int xhi = x + width;
+                        int ylo = y;
+                        int yhi = y + height;
+                        int[] xs = new int[] { xlo, xhi, xlo, };
+                        int[] ys = new int[] { ylo, y + height / 2, yhi, };
+                        g.fillPolygon( xs, ys, 3 );
+                    }
+                    public int getIconWidth() {
+                        return width;
+                    }
+                    public int getIconHeight() {
+                        return height;
+                    }
+                };
+            }
+        }
+        public String getToolTipText( Object value ) {
+            if ( value instanceof Transmission ) {
+                return getToolTipText( (Transmission) value );
+            }
+            else {
+                return null;
+            }
+        }
     }
 }
