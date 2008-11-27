c792b48a0beb808a37495c7b9a041346619eb637
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-11-27 14:53:18 +0000

    Transmission renderer
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@24904 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/gui/TransmissionCellRenderer.java
@@ -0,0 +1,62 @@
+package org.astrogrid.samp.gui;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.Icon;
+
+/**
+ * CellRenderer for transmission objects.
+ *
+ * @author   Mark Taylor
+ * @since    27 Nov 2008
+ */
+class TransmissionCellRenderer implements IconBox.CellRenderer {
+
+    public Icon getIcon( IconBox iconBox, Object value, int index ) {
+        Insets insets = iconBox.getInsets();
+        final int size = iconBox.getTransverseSize()
+                       - insets.top - insets.bottom;
+        if ( value instanceof Transmission ) {
+            final Transmission trans = (Transmission) value;
+            final int width = (int) Math.floor( 0.866 * size );
+            return new Icon() {
+                public void paintIcon( Component c, Graphics g, int x, int y ) {
+                    int xlo = x;
+                    int xhi = x + size;
+                    int[] xs = new int[] { x, x + width, x, };
+                    int[] ys = new int[] { y, y + size / 2, y + size, };
+                    g.fillPolygon( xs, ys, 3 );
+                }
+                public int getIconWidth() {
+                    return width;
+                }
+                public int getIconHeight() {
+                    return size;
+                }
+            };
+        }
+        else {
+            return new Icon() {
+                public void paintIcon( Component c, Graphics g, int x, int y ) {
+                    g.drawOval( x, y, size, size );
+                }
+                public int getIconWidth() {
+                    return size;
+                }
+                public int getIconHeight() {
+                    return size;
+                }
+            };
+        }
+    }
+
+    public String getToolTipText( IconBox iconBox, Object value, int index ) {
+        if ( value instanceof Transmission ) {
+            return ((Transmission) value).getMessage().getMType();
+        }
+        else {
+            return null;
+        }
+    }
+}
