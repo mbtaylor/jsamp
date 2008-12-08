81f906fff79d22885fd99d281fd1b44e1334368a
Mark Taylor <m.b.taylor@bristol.ac.uk>
2008-12-08 16:21:33 +0000

    Add -mtype option
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@24995 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/gui/HubMonitor.java
+++ src/java/org/astrogrid/samp/gui/HubMonitor.java
@@ -3,8 +3,10 @@ package org.astrogrid.samp.gui;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.Box;
@@ -14,8 +16,16 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import org.astrogrid.samp.ErrInfo;
+import org.astrogrid.samp.Message;
 import org.astrogrid.samp.Metadata;
+import org.astrogrid.samp.Response;
+import org.astrogrid.samp.Subscriptions;
 import org.astrogrid.samp.client.ClientProfile;
+import org.astrogrid.samp.client.HubConnection;
+import org.astrogrid.samp.client.HubConnector;
+import org.astrogrid.samp.client.MessageHandler;
+import org.astrogrid.samp.client.SampException;
 import org.astrogrid.samp.xmlrpc.StandardClientProfile;
 import org.astrogrid.samp.xmlrpc.XmlRpcKit;
 
@@ -29,6 +39,7 @@ import org.astrogrid.samp.xmlrpc.XmlRpcKit;
  */
 public class HubMonitor extends JPanel {
 
+    private final GuiHubConnector connector_;
     private static Logger logger_ =
         Logger.getLogger( HubMonitor.class.getName() );
 
@@ -46,14 +57,13 @@ public class HubMonitor extends JPanel {
         super( new BorderLayout() );
 
         // Set up a new GuiHubConnector and GUI decorations.
-        GuiHubConnector connector =
-            trackMessages ? new MessageTrackerHubConnector( profile )
-                          : new GuiHubConnector( profile );
+        connector_ = trackMessages ? new MessageTrackerHubConnector( profile )
+                                   : new GuiHubConnector( profile );
 
         // Declare the default subscriptions.  This is required so that
         // the hub knows the client is subscribed to those hub.event
         // MTypes which inform about client registration, hub shutdown etc.
-        connector.declareSubscriptions( connector.computeSubscriptions() );
+        connector_.declareSubscriptions( connector_.computeSubscriptions() );
 
         // Declare metadata about this application.
         Metadata meta = new Metadata();
@@ -62,12 +72,12 @@ public class HubMonitor extends JPanel {
         meta.setIconUrl( "http://www.star.bristol.ac.uk/~mbt/"
                        + "plastic/images/eye.gif" );
         meta.put( "author", "Mark Taylor" );
-        connector.declareMetadata( meta );
+        connector_.declareMetadata( meta );
 
         // Create and place a component which maintains a display of 
         // currently registered clients.  A more modest GUI could just use
         // connector.getClientListModel() as a model for a JList component.
-        add( connector.createMonitorPanel(), BorderLayout.CENTER );
+        add( connector_.createMonitorPanel(), BorderLayout.CENTER );
 
         // Prepare a container for other widgets at the bottom of the window.
         JPanel infoBox = new JPanel( new BorderLayout() );
@@ -76,26 +86,35 @@ public class HubMonitor extends JPanel {
         // Create and place components which allow the user to 
         // view and control registration/unregistration explicitly.
         JComponent connectBox = new JPanel( new BorderLayout() );
-        connectBox.add( new JButton( connector.createToggleRegisterAction() ),
+        connectBox.add( new JButton( connector_.createToggleRegisterAction() ),
                         BorderLayout.CENTER );
-        connectBox.add( connector.createConnectionIndicator(),
+        connectBox.add( connector_.createConnectionIndicator(),
                         BorderLayout.EAST );
         infoBox.add( connectBox, BorderLayout.EAST );
 
         // Create and place components which provide a compact display 
         // of the connector's status.
         JComponent statusBox = Box.createHorizontalBox();
-        statusBox.add( connector.createClientBox( false, 24 ) );
-        if ( connector instanceof MessageTrackerHubConnector ) {
-            statusBox.add( ((MessageTrackerHubConnector) connector)
+        statusBox.add( connector_.createClientBox( false, 24 ) );
+        if ( connector_ instanceof MessageTrackerHubConnector ) {
+            statusBox.add( ((MessageTrackerHubConnector) connector_)
                           .createMessageBox( 24 ) );
         }
         infoBox.add( statusBox, BorderLayout.CENTER );
 
         // Attempt registration, and arrange that if/when unregistered we look
         // for a hub to register with on a regular basis.
-        connector.setActive( true );
-        connector.setAutoconnect( autoSec );
+        connector_.setActive( true );
+        connector_.setAutoconnect( autoSec );
+    }
+
+    /**
+     * Returns this monitor's HubConnector.
+     *
+     * @return  hub connector
+     */
+    public GuiHubConnector getHubConnector() {
+        return connector_;
     }
 
     /**
@@ -114,6 +133,8 @@ public class HubMonitor extends JPanel {
             .append( " [-auto <secs>]" )
             .append( " [-nomsg]" )
             .append( " [-nogui]" )
+            .append( "\n           " )
+            .append( " [-mtype <pattern>]" )
             .append( "\n" )
             .toString();
         List argList = new ArrayList( Arrays.asList( args ) );
@@ -122,6 +143,7 @@ public class HubMonitor extends JPanel {
         boolean trackMsgs = true;
         int autoSec = 3;
         XmlRpcKit xmlrpc = null;
+        Subscriptions subs = new Subscriptions();
         for ( Iterator it = argList.iterator(); it.hasNext(); ) {
             String arg = (String) it.next();
             if ( arg.startsWith( "-auto" ) && it.hasNext() ) {
@@ -145,6 +167,12 @@ public class HubMonitor extends JPanel {
             else if ( arg.equals( "-nomsg" ) ) {
                 it.remove();
                 trackMsgs = false;
+            }
+            else if ( arg.startsWith( "-mtype" ) && it.hasNext() ) {
+                it.remove();
+                String mpat = (String) it.next();
+                it.remove();
+                subs.addMType( mpat );
             }
             else if ( arg.equals( "-xmlrpc" ) && it.hasNext() ) {
                 it.remove();
@@ -196,16 +224,43 @@ public class HubMonitor extends JPanel {
             xmlrpc == null ? StandardClientProfile.getInstance()
                            : new StandardClientProfile( xmlrpc );
 
+        // Create the HubMonitor.
+        final HubMonitor monitor =
+            new HubMonitor( profile, trackMsgs, autoSec );
+
+        // Add a handler for extra MTypes if so requested.
+        if ( ! subs.isEmpty() ) {
+            final Subscriptions extraSubs = subs;
+            HubConnector connector = monitor.getHubConnector();
+            final Response dummyResponse = new Response();
+            dummyResponse.setStatus( Response.WARNING_STATUS );
+            dummyResponse.setResult( new HashMap() );
+            dummyResponse.setErrInfo( new ErrInfo( "Message logged, "
+                                                 + "no other action taken" ) );
+            connector.addMessageHandler( new MessageHandler() {
+                public Map getSubscriptions() {
+                    return extraSubs;
+                }
+                public void receiveNotification( HubConnection connection,
+                                                 String senderId,
+                                                 Message msg ) {
+                }
+                public void receiveCall( HubConnection connection,
+                                         String senderId, String msgId,
+                                         Message msg )
+                        throws SampException {
+                    connection.reply( msgId, dummyResponse );
+                }
+            } );
+            connector.declareSubscriptions( connector.computeSubscriptions() );
+        }
+
         // Start the gui in a new window.
         final boolean isVisible = gui;
-        final int autoSeconds = autoSec;
-        final boolean trackMessages = trackMsgs;
         SwingUtilities.invokeLater( new Runnable() {
             public void run() {
                 JFrame frame = new JFrame( "SAMP HubMonitor" );
-                frame.getContentPane()
-                     .add( new HubMonitor( profile, trackMessages,
-                                           autoSeconds ) );
+                frame.getContentPane().add( monitor );
                 frame.setIconImage(
                     new ImageIcon( Metadata.class
                                   .getResource( "images/eye.gif" ) )
