b9e9305040549209931807d5f66d3e28495278ee
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-01 11:59:50 +0000

    Interface for specifying hub characteristics from the command line
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26864 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/hub/HubProfileFactory.java
@@ -0,0 +1,45 @@
+package org.astrogrid.samp.hub;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Factory to produce hub profiles of a particular type.
+ * Used with the command-line invocation of the hub.
+ *
+ * @author   Mark Taylor
+ * @since    31 Jan 2011
+ */
+public interface HubProfileFactory {
+
+    /**
+     * Returns the name used to identify this profile.
+     *
+     * @return  short name
+     */
+    String getName();
+
+    /**
+     * Returns an array of strings, each describing one command-line flag
+     * which will be consumed by the <code>createProfile</code> method.
+     *
+     * @return  array of plain-text strings suitable for use as part of
+     *          a usage message
+     */
+    String[] getFlagsUsage();
+
+    /**
+     * Creates a HubProfile perhaps configured using a supplied list
+     * of flags.  Any flags which match those described by the
+     * {@link #getFlagsUsage} command are used for configuration of the
+     * returned hub, and must be removed from the <code>flagList</code> list.
+     * Unrecognised flags should be ignored and left in the list.
+     * Flags which are recognised but badly formed should raise a
+     * RuntimeException with a helpful message.
+     *
+     * @param  flagList  mutable list of Strings giving command-ilne flags,
+     *         some of which may be intended for configuring a profile
+     * @return  new profile
+     */
+    HubProfile createHubProfile( List flagList ) throws IOException;
+}
