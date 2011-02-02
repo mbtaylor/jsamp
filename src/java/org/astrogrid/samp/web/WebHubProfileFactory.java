10bdeba313236f3d8bda81a9f0d9bff06a3de30f
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-02 18:00:52 +0000

    WebHubProfile factory for use on command line
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26884 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/web/WebHubProfileFactory.java
@@ -0,0 +1,106 @@
+package org.astrogrid.samp.web;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import org.astrogrid.samp.hub.HubProfile;
+import org.astrogrid.samp.hub.HubProfileFactory;
+import org.astrogrid.samp.hub.KeyGenerator;
+import org.astrogrid.samp.xmlrpc.internal.InternalServer;
+
+/**
+ * HubProfileFactory implementation for Web Profile.
+ *
+ * @author   Mark Taylor
+ * @since    2 Feb 2011
+ */
+public class WebHubProfileFactory implements HubProfileFactory {
+
+    private static final String logUsage_ = "[-web:log none|http|xml|rpc]";
+    private static final String authUsage_ = "[-web:auth swing|true|false]";
+    private static final String remoteUsage_ = "[-web:allowremote]";
+
+    /**
+     * Returns "web".
+     */
+    public String getName() {
+        return "web";
+    }
+
+    public String[] getFlagsUsage() {
+        return new String[] {
+            logUsage_,
+            authUsage_,
+            remoteUsage_,
+        };
+    }
+
+    public HubProfile createHubProfile( List flagList ) throws IOException {
+
+        // Process flags.
+        String logType = "none";
+        String authType = "swing";
+        boolean allowRemote = false;
+        for ( Iterator it = flagList.iterator(); it.hasNext(); ) {
+            String arg = (String) it.next();
+            if ( arg.equals( "-web:log" ) ) {
+                it.remove();
+                if ( it.hasNext() ) {
+                    logType = (String) it.next();
+                    it.remove();
+                }
+                else {
+                    throw new IllegalArgumentException( "Usage: " + logUsage_ );
+                }
+            }
+            else if ( arg.equals( "-web:auth" ) ) {
+                it.remove();
+                if ( it.hasNext() ) {
+                    authType = (String) it.next();
+                    it.remove();
+                }
+                else {
+                    throw new IllegalArgumentException( "Usage: "
+                                                      + authUsage_ );
+                }
+            }
+            else if ( arg.equals( "-web:allowremote" ) ) {
+                it.remove();
+                allowRemote = true;
+            }
+            else if ( arg.equals( "-web:noallowremote" ) ) {
+                it.remove();
+                allowRemote = false;
+            }
+        }
+
+        // Prepare HTTP server.
+        InternalServer xServer;
+        try {
+            xServer = WebHubProfile.createSampXmlRpcServer( logType );
+        }
+        catch ( IllegalArgumentException e ) {
+            throw (IllegalArgumentException)
+                  new IllegalArgumentException( "Unknown log type " + logType
+                                              + "; Usage: " + logUsage_ )
+                 .initCause( e );
+        }
+
+        // Prepare client authorizer.
+        ClientAuthorizer clientAuth;
+        try {
+            clientAuth = ClientAuthorizers.getClientAuthorizer( authType );
+        }
+        catch ( IllegalArgumentException e ) {
+            throw (IllegalArgumentException)
+                  new IllegalArgumentException( "Unknown authorizer type "
+                                              + authType + "; Usage: "
+                                              + authUsage_ )
+                 .initCause( e );
+        }
+
+        // Construct and return an appropriately configured hub profile.
+        return new WebHubProfile( xServer, clientAuth,
+                                  WebHubProfile.createKeyGenerator() );
+    }
+}
