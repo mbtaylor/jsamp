7b69791aee8b94b8ebaee2e40e9cc99bfdb5dae5
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-02 17:56:43 +0000

    Initial version just contains some constants
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26880 d07e2994-6e00-4123-b050-8522f38abc7a

---
+++ src/java/org/astrogrid/samp/web/WebClientProfile.java
@@ -0,0 +1,9 @@
+package org.astrogrid.samp.web;
+
+public class WebClientProfile {
+    public static final int WEBSAMP_PORT = 21012;
+    public static final String WEBSAMP_PATH = "/";
+    public static final String WEBSAMP_HUB_PREFIX = "samp.webhub.";
+    public static final String WEBSAMP_CLIENT_PREFIX = "";
+    public static final String URLTRANS_KEY = "samp.url-translator";
+}
