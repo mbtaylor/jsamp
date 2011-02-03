27233e60336c015626d3b6aab8f6cf862e8ba85e
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-03 16:34:59 +0000

    make sure clients are notified on facade service shutdown
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26911 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/hub/FacadeHubService.java
+++ src/java/org/astrogrid/samp/hub/FacadeHubService.java
@@ -1,5 +1,14 @@
 package org.astrogrid.samp.hub;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+import org.astrogrid.samp.Message;
+import org.astrogrid.samp.RegInfo;
+import org.astrogrid.samp.Subscriptions;
+import org.astrogrid.samp.client.CallableClient;
 import org.astrogrid.samp.client.ClientProfile;
 import org.astrogrid.samp.client.HubConnection;
 import org.astrogrid.samp.client.SampException;
@@ -15,6 +24,9 @@ import org.astrogrid.samp.client.SampException;
 public class FacadeHubService implements HubService {
 
     private final ClientProfile profile_;
+    private final Set connectionSet_;
+    private static final Logger logger_ =
+        Logger.getLogger( FacadeHubService.class.getName() );
 
     /**
      * Constructor.
@@ -24,6 +36,7 @@ public class FacadeHubService implements HubService {
      */
     public FacadeHubService( ClientProfile profile ) {
         profile_ = profile;
+        connectionSet_ = Collections.synchronizedSet( new HashSet() );
     }
 
     public boolean isHubRunning() {
@@ -31,7 +44,37 @@ public class FacadeHubService implements HubService {
     }
 
     public HubConnection register() throws SampException {
-        return profile_.register();
+
+        // Mostly delegate registration to the underlying client profile,
+        // but put in place machinery to keep track of which clients
+        // are registered via this service.  This will be required so that
+        // they can be messaged if the underlying hub shuts down.
+        HubConnection baseConnection = profile_.register();
+        if ( baseConnection != null ) {
+            HubConnection conn = new FacadeHubConnection( baseConnection ) {
+                final HubConnection hubConn = this;
+                public void ping() throws SampException {
+                    if ( FacadeHubService.this.isHubRunning() ) {
+                        super.ping();
+                    }
+                    else {
+                        throw new SampException( "Hub underlying facade "
+                                               + "is not running" );
+                    }
+                }
+                public void unregister() throws SampException {
+                    connectionSet_.remove( hubConn );
+                    super.unregister();
+                }
+            };
+            connectionSet_.add( conn );
+            return conn;
+        }
+
+        // Or return null if there is no underlying hub.
+        else {
+            return null;
+        }
     }
 
     /**
@@ -40,9 +83,75 @@ public class FacadeHubService implements HubService {
     public void start() {
     }
 
-    /**
-     * No-op.
-     */
     public void shutdown() {
+        hubEvent( new Message( "samp.hub.event.shutdown" ) );
+        connectionSet_.clear();
+    }
+
+    /**
+     * Sends a given message by notification, as if from the hub,
+     * to all the clients which have registered through this service.
+     *
+     * @param  msg  message to send
+     */
+    private void hubEvent( Message msg ) {
+        String mtype = msg.getMType();
+        FacadeHubConnection[] connections =
+            (FacadeHubConnection[])
+            connectionSet_.toArray( new FacadeHubConnection[ 0 ] );
+        for ( int ic = 0; ic < connections.length; ic++ ) {
+            connections[ ic ].hubEvent( msg );
+        }
+    }
+
+    /**
+     * Utility HubConnection class which allows hub event notifications
+     * to be sent to clients.
+     */
+    private static class FacadeHubConnection extends WrapperHubConnection {
+        private CallableClient callable_;
+        private Subscriptions subs_;
+
+        /**
+         * Constructor.
+         *
+         * @param   base  base connection
+         */
+        FacadeHubConnection( HubConnection base ) {
+            super( base );
+        }
+
+        public void setCallable( CallableClient callable )
+                throws SampException {
+            super.setCallable( callable );
+            callable_ = callable;
+        }
+
+        public void declareSubscriptions( Map subs ) throws SampException {
+            super.declareSubscriptions( subs );
+            subs_ = subs == null ? null
+                                 : Subscriptions.asSubscriptions( subs );
+        }
+
+        /**
+         * Sends a given message as a notification, as if from the hub,
+         * to this connection if it is able to receive it.
+         *
+         * @param  msg  message to send
+         */
+        void hubEvent( Message msg ) {
+            String mtype = msg.getMType();
+            CallableClient callable = callable_;
+            if ( callable != null && subs_.isSubscribed( mtype ) ) {
+                RegInfo regInfo = getRegInfo();
+                try {
+                    callable.receiveNotification( regInfo.getHubId(), msg );
+                }
+                catch ( Throwable e ) {
+                    logger_.info( "Failed " + mtype + " notification to "
+                                + regInfo.getSelfId() );
+                }
+            }
+        }
     }
 }
