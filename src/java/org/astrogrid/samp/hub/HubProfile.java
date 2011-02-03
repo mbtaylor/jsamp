1b7821c017eba8f0683e3f51016295ea4255c903
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-03 17:49:39 +0000

    HubProfile now operates on a ClientProfile not a HubService, which allows more general implementations
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26914 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/hub/HubProfile.java
+++ src/java/org/astrogrid/samp/hub/HubProfile.java
@@ -2,14 +2,17 @@ package org.astrogrid.samp.hub;
 
 import java.io.IOException;
 import java.util.List;
+import org.astrogrid.samp.client.ClientProfile;
 
 /**
  * Defines a hub profile.
  * This profile allows registration and deregistration of clients to
- * a given HubService, using some profile-specific transport and
- * authentication arrangements.  Multiple profiles may be attached to a single
- * HubService at any time, and may be started and stopped independently of
- * each other.
+ * a given provider of hub connections, using some profile-specific
+ * transport and authentication arrangements.
+ * Multiple profiles may be attached to a single connection supplier
+ * at any time, and may be started and stopped independently of each other.
+ * The connection supplier is typically a hub service running in the same
+ * JVM, but may also be a client-side connection to a hub.
  * 
  * @author   Mark Taylor
  * @since    31 Jan 2011
@@ -17,16 +20,18 @@ import java.util.List;
 public interface HubProfile {
 
     /**
-     * Starts this hub's activity allowing access to a given running
-     * hub service.
+     * Starts this hub's activity allowing access to a given supplier of
+     * hub connections.
      *
-     * @param   service  object providing hub services
+     * @param   profile  object which can provide hub connections
      */
-    void start( HubService service ) throws IOException;
+    void start( ClientProfile profile ) throws IOException;
 
     /**
      * Ends this profile's activity on behalf of the hub.
      * Any resources associated with the profile should be released.
+     * This does not include messaging registered clients about profile
+     * termination; that should be taken care of by the user of this profile.
      */
     void shutdown() throws IOException;
 }
