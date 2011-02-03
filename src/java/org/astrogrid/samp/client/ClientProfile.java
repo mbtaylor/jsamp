2ca47b5bcd6f12c2f7b986947d9d81eee5cf3977
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-03 17:47:58 +0000

    Generalise the semantics of the ClientProfile interface
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26912 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/client/ClientProfile.java
+++ src/java/org/astrogrid/samp/client/ClientProfile.java
@@ -1,9 +1,18 @@
 package org.astrogrid.samp.client;
 
 /**
- * Defines Profile-specific aspects of the SAMP client implementation.
- * The usual way for clients to obtain an instance of this class is by 
- * using {@link DefaultClientProfile#getProfile}.
+ * Defines an object that can be used to register with a running SAMP hub.
+ * Registration takes the form of providing a connection object which
+ * a client can use to perform further hub interactions.
+ * Client-side implementations will take care of communications,
+ * mapping between profile-specific transport mechanisms and the
+ * methods of the generated {@link HubConnection} objects.
+ *
+ * <p>The usual way for a prospective SAMP client to obtain an instance of 
+ * this class is by using {@link DefaultClientProfile#getProfile}.
+ *
+ * <p>This interface is so-named partly for historical reasons;
+ * "HubConnectionFactory" might have been more appropriate.
  *
  * @author   Mark Taylor
  * @since    15 Jul 2008
@@ -25,7 +34,7 @@ public interface ClientProfile {
      * Indicates whether a hub contactable by this profile appears to be
      * running.  This method is intended to provide a best guess and
      * to execute reasonably quickly.  An appropriate implementation for
-     * standard profile might be to check the existence of the lockfile,
+     * the Standard Profile might be to check the existence of the lockfile,
      * but not to attempt contact or registration with the hub.
      *
      * @return  true iff it looks like a hub is running
