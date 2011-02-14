920df3f6b7e6e24debd237585a057c4a0bb847cc
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-02-14 17:10:46 +0000

    Use one thread per incoming CallableClient XML-RPC call (return type is void in each case) to ensure fast turnaround of the HTTP connection.
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@26976 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/java/org/astrogrid/samp/xmlrpc/ClientXmlRpcHandler.java
+++ src/java/org/astrogrid/samp/xmlrpc/ClientXmlRpcHandler.java
@@ -4,9 +4,13 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.astrogrid.samp.ErrInfo;
 import org.astrogrid.samp.Message;
 import org.astrogrid.samp.Response;
 import org.astrogrid.samp.client.CallableClient;
+import org.astrogrid.samp.client.HubConnection;
 
 /**
  * SampXmlRpcHandler implementation which passes Standard Profile-like XML-RPC
@@ -19,6 +23,8 @@ import org.astrogrid.samp.client.CallableClient;
 class ClientXmlRpcHandler extends ActorHandler {
 
     private final ClientActorImpl clientActor_;
+    private static final Logger logger_ = 
+        Logger.getLogger( ClientXmlRpcHandler.class.getName() );
 
     /**
      * Constructor.
@@ -31,21 +37,23 @@ class ClientXmlRpcHandler extends ActorHandler {
     /**
      * Adds a CallableClient object to this server.
      *
-     * @param   privateKey  private key for the registered client on behalf
+     * @param   connection  hub connection for the registered client on behalf
      *          of which the client will operate
      * @param   callable   callable client object
      */
-    public void addClient( String privateKey, CallableClient callable ) {
-        clientActor_.map_.put( privateKey, callable );
+    public void addClient( HubConnection connection, CallableClient callable ) {
+        clientActor_.entryMap_.put( connection.getRegInfo().getPrivateKey(),
+                                    new Entry( connection, callable ) );
     }
 
     /**
      * Removes a CallableClient object from this server.
      *
-     * @param  privateKey   key under which this client was added
+     * @param  privateKey   hub connection for which this client was added
      */
-    public void removeClient( String privateKey ) {
-        clientActor_.map_.remove( privateKey );
+    public void removeClient( HubConnection connection ) {
+        clientActor_.entryMap_
+                    .remove( connection.getRegInfo().getPrivateKey() );
     }
 
     protected Object invokeMethod( Method method, Object obj, Object[] args )
@@ -61,43 +69,111 @@ class ClientXmlRpcHandler extends ActorHandler {
      */
     private static class ClientActorImpl implements ClientActor {
 
-        private final Map map_ = new HashMap();
+        private Map entryMap_ = new HashMap();  // String -> Entry
 
-        public void receiveNotification( String privateKey, String senderId,
-                                         Map msg ) throws Exception {
-            getCallable( privateKey )
-                .receiveNotification( senderId, Message.asMessage( msg ) );
+        public void receiveNotification( String privateKey,
+                                         final String senderId, Map msg ) {
+            Entry entry = getEntry( privateKey );
+            final CallableClient callable = entry.callable_;
+            final Message message = Message.asMessage( msg );
+            final String label = "Notify " + senderId + " "
+                               + message.getMType();
+            new Thread( label ) {
+                public void run() {
+                    try {
+                        callable.receiveNotification( senderId, message );
+                    }
+                    catch ( Throwable e ) {
+                        logger_.log( Level.INFO, label + " error", e );
+                    }
+                }
+            }.start();
         }
 
-        public void receiveCall( String privateKey, String senderId,
-                                 String msgId, Map msg ) throws Exception {
-            getCallable( privateKey )
-                .receiveCall( senderId, msgId, Message.asMessage( msg ) );
+        public void receiveCall( String privateKey, final String senderId,
+                                 final String msgId, Map msg )
+                throws Exception {
+            Entry entry = getEntry( privateKey );
+            final CallableClient callable = entry.callable_;
+            final HubConnection connection = entry.connection_;
+            final Message message = Message.asMessage( msg );
+            final String label = "Call " + senderId + " " + message.getMType();
+            new Thread( label ) {
+                public void run() {
+                    try {
+                        callable.receiveCall( senderId, msgId, message );
+                    }
+                    catch ( Throwable e ) {
+                        try {
+                            Response response =
+                                Response
+                               .createErrorResponse( new ErrInfo( e ) );
+                            connection.reply( msgId, response );
+                        }
+                        catch ( Throwable e2 ) {
+                            logger_.log( Level.INFO,
+                                         label + " error replying", e2 );
+                        }
+                    }
+                }
+            }.start();
         }
 
-        public void receiveResponse( String privateKey, String responderId,
-                                     String msgTag, Map response )
+        public void receiveResponse( String privateKey,
+                                     final String responderId,
+                                     final String msgTag, Map resp )
                  throws Exception {
-            getCallable( privateKey )
-                .receiveResponse( responderId, msgTag,
-                                  Response.asResponse( response ) );
+            Entry entry = getEntry( privateKey );
+            final CallableClient callable = entry.callable_;
+            final Response response = Response.asResponse( resp );
+            final String label = "Reply " + responderId;
+            new Thread( label ) {
+                public void run() {
+                    try {
+                        callable.receiveResponse( responderId, msgTag,
+                                                  response );
+                    }
+                    catch ( Throwable e ) {
+                        logger_.log( Level.INFO, label + " error replying", e );
+                    }
+                }
+            }.start();
         }
 
         /**
          * Returns the CallableClient corresponding to a given private key.
          *
          * @param   privateKey  private key for client
-         * @return  callable client identified by privateKey
+         * @return  entry identified by privateKey
          * @throws  IllegalStateException  if <code>privateKey</code> is unknown
          */
-        private CallableClient getCallable( String privateKey ) {
-            Object cc = map_.get( privateKey );
-            if ( cc instanceof CallableClient ) {
-                return (CallableClient) cc;
+        private Entry getEntry( String privateKey ) {
+            Object ent = entryMap_.get( privateKey );
+            if ( ent instanceof Entry ) {
+                return (Entry) ent;
             }
             else {
                 throw new IllegalStateException( "Client is not listening" );
             }
+        }
+    }
+
+    /**
+     * Utility class to aggregate information about a client.
+     */
+    private static class Entry {
+        final HubConnection connection_;
+        final CallableClient callable_;
+
+        /**
+         * Constructor.
+         *
+         * @param   connection  hub connection
+         * @param   callable  callable client
+         */
+        Entry( HubConnection connection, CallableClient callable ) {
+            connection_ = connection;
+            callable_ = callable;
         }
     }
 }
