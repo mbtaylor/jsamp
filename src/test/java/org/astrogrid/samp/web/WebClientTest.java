06c5adbe31f82af4ee3adae7537e2cb99fa274ef
Mark Taylor <m.b.taylor@bristol.ac.uk>
2011-07-15 11:46:53 +0000

    add test to do with allowReverseCallbacks; currently unused but code may be useful one day
    git-svn-id: file:///mbt/data/jsamp/svn-mirror/trunk/astrogrid/samp@27136 d07e2994-6e00-4123-b050-8522f38abc7a

--- src/test/java/org/astrogrid/samp/web/WebClientTest.java
+++ src/test/java/org/astrogrid/samp/web/WebClientTest.java
@@ -7,16 +7,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import junit.framework.TestCase;
+import org.astrogrid.samp.Message;
 import org.astrogrid.samp.RegInfo;
+import org.astrogrid.samp.Response;
 import org.astrogrid.samp.SampUtils;
+import org.astrogrid.samp.Subscriptions;
 import org.astrogrid.samp.TestProfile;
+import org.astrogrid.samp.client.CallableClient;
 import org.astrogrid.samp.client.HubConnection;
 import org.astrogrid.samp.client.SampException;
 import org.astrogrid.samp.httpd.HttpServer;
+import org.astrogrid.samp.xmlrpc.XmlRpcHubConnection;
 import org.astrogrid.samp.xmlrpc.internal.InternalServer;
 
 public class WebClientTest extends TestCase {
@@ -26,6 +34,37 @@ public class WebClientTest extends TestCase {
         Logger.getLogger( InternalServer.class.getName() )
               .setLevel( Level.SEVERE );
     }
+
+    // Test withdrawn, multiple cycles of allowReverseCallbacks are not
+    // recommended.  Code may come in useful for something else another
+    // day though.
+    public void doNottestCallbacks() throws IOException, InterruptedException {
+        WebTestProfile profile = new WebTestProfile( new Random( 23L ) );
+        profile.startHub();
+        XmlRpcHubConnection tconn = (XmlRpcHubConnection) profile.register();
+        XmlRpcHubConnection rconn = (XmlRpcHubConnection) profile.register();
+        String tid = tconn.getRegInfo().getSelfId();
+        String rid = rconn.getRegInfo().getSelfId();
+        Subscriptions subs = new Subscriptions();
+        String pMtype = "test.ping";
+        subs.addMType( pMtype );
+        Message pmsg = new Message( pMtype );
+        Receiver rcvr = new Receiver();
+        rconn.setCallable( rcvr );
+        rconn.declareSubscriptions( subs );
+        
+        tconn.notify( rid, pmsg );
+        assertTrue( rcvr.waitForMessage( 1000 ) != null );
+        rconn.exec( "allowReverseCallbacks", new String[] { "0" } );
+        tconn.notify( rid, pmsg );
+        assertTrue( rcvr.waitForMessage( 1000 ) == null );
+        rconn.exec( "allowReverseCallbacks", new String[] { "1" } );
+        assertTrue( rcvr.waitForMessage( 1000 ) != null );
+        assertTrue( rcvr.waitForMessage( 1000 ) == null );
+        tconn.unregister();
+        rconn.unregister();
+    }
+
 
     public void testUrlTranslator() throws IOException {
         WebTestProfile profile = new WebTestProfile( new Random( 23L ) );
@@ -83,5 +122,45 @@ public class WebClientTest extends TestCase {
         }
         in.close();
         return ubuf.toString();
+    }
+
+    private static class Receiver implements CallableClient {
+        private final List msgList_ = new ArrayList();
+        private final List responseList_ = new ArrayList();
+        public synchronized void receiveCall( String senderId, String msgId,
+                                              Message msg ) {
+            msgList_.add( msg );
+            notifyAll();
+        }
+        public synchronized void receiveNotification( String senderId,
+                                                      Message msg ) {
+            msgList_.add( msg );
+            notifyAll();
+        }
+        public synchronized void receiveResponse( String responsderId,
+                                                  String msgTag,
+                                                  Response response ) {
+            msgList_.add( response );
+            notifyAll();
+        }
+        public synchronized Message waitForMessage( long millis )
+                throws InterruptedException {
+            long end = System.currentTimeMillis() + millis;
+            while ( System.currentTimeMillis() < end && msgList_.isEmpty() ) {
+                wait( end - System.currentTimeMillis() );
+            }
+            return msgList_.isEmpty() ? null
+                                      : (Message) msgList_.get( 0 );
+        }
+        public synchronized Response waitForResponse( long millis )
+                throws InterruptedException {
+            long end = System.currentTimeMillis() + millis;
+            while ( System.currentTimeMillis() < end &&
+                    responseList_.isEmpty() ) {
+                wait( end - System.currentTimeMillis() );
+            }
+            return responseList_.isEmpty() ? null
+                                           : (Response) responseList_.get( 0 );
+        }
     }
 }
